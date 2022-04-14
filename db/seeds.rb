64ee0be2028d1df635ecf59132ce5fbcc6bd22bb
Ryan Searls <Ryan.searls@gmail.com>
2022-04-13 21:46:59 -0600

    creates csv file in lib/tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib/tasks', 'seeds', 'sample_campground_data.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  t = Transaction.new
+  t.name = row['name']
+  t.campsites = row['campsites']
+  t.price = row['price']
+  t.save
+  puts "#{t.name}, #{t.campsites} saved"
+end 
+puts csv_text
